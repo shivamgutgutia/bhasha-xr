18eb29ceeeafb5a53e1799f504e49a8eb5f1f5a0
Shivam Gutgutia <gutgutia159@gmail.com>
2024-04-14 12:12:08 +0530

    Change: float array to byte array in evaluate2

--- controllers/evaluate2.py
+++ controllers/evaluate2.py
@@ -5,20 +5,16 @@ import numpy as np
 import scipy.io.wavfile as wav
 import json
 
-def float_to_wav(float_data, file_path, sample_rate=44100):
-    # Normalize float data to the range [-1, 1]
-    normalized_data = np.int16(float_data / np.max(np.abs(float_data)) * 32767)
+def float_to_wav(byte_data, file_path, sample_rate=44100):
     
-    # Write the data to a WAV file
     with wav.Wave_write(file_path) as wf:
-        wf.setnchannels(1)  # Mono
-        wf.setsampwidth(2)  # 2 bytes (16 bits) per sample
+        wf.setnchannels(1) 
+        wf.setsampwidth(2) 
         wf.setframerate(sample_rate)
-        wf.writeframes(normalized_data)
+        wf.writeframes(byte_data)
 
 def evaluate2():
     client = OpenAI()
-    #t = Translator(service_urls=["translate.google.co.in"])
     audio = json.loads(request.form.get("audio"))
     float_to_wav(audio,"./upload/uploaded.wav")
     file = open("./upload/uploaded.wav","rb")
